d4bf5c831d87e0487c776a87b4fdcd012efdc266
deathcap <deathcap@gmx.co.uk>
2015-02-22 22:24:33 -0800

    Async cat

--- src/cat.js
+++ src/cat.js
@@ -16,28 +16,39 @@ var fs = require('fs');
 //@ Returns a string containing the given file, or a concatenated string
 //@ containing the files if more than one file is given (a new line character is
 //@ introduced between each file). Wildcard `*` accepted.
-function _cat(options, files) {
+function _cat(options, files, cb) {
   var cat = '';
 
   if (!files)
-    common.error('no paths given');
+    return cb(new Error('no paths given'));
 
   if (typeof files === 'string')
     files = [].slice.call(arguments, 1);
   // if it's array leave it as it is
 
-  files = common.expand(files);
+  //files = common.expand(files); // TODO: async
 
+  var completed = 0;
   files.forEach(function(file) {
-    if (!fs.existsSync(file))
-      common.error('no such file or directory: ' + file);
+    fs.exists(file, function(exists) {
+      if (!exists)
+        return cb(new Error('no such file or directory: ' + file));
 
-    cat += fs.readFileSync(file, 'utf8') + '\n';
+      fs.readFile(file, 'utf8', function(err, data) {
+        if (err) return cb(err);
+
+        cat += data + '\n';
+
+        completed += 1;
+        if (completed === files.length) { // TODO: collect
+          if (cat[cat.length-1] === '\n')
+            cat = cat.substring(0, cat.length-1);
+
+          cb(null, common.ShellString(cat));
+        }
+      });
+    });
   });
 
-  if (cat[cat.length-1] === '\n')
-    cat = cat.substring(0, cat.length-1);
-
-  return common.ShellString(cat);
 }
 module.exports = _cat;
